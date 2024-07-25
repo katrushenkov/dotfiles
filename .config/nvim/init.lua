0d9fca2961b692bf013192418e783ceb285def7c
katrushenkov <katrushenkov.sm@dns-shop.ru>
2024-07-25 13:16:02 +1000

    move to AstroNvim configs

---
+++ .config/nvim/init.lua
@@ -0,0 +1,126 @@
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+--require "polish"
+
+require("telescope").setup {
+  extensions = {
+    project = {
+      base_dirs = {
+        --{ "~/.local/src/datagrip/org" },
+        --{ "~/.local/src" },
+        --        { "~/dev/src3", max_depth = 4 },
+        { path = "~/.local/src" },
+        --        { path = "~/dev/src5", max_depth = 2 },
+      },
+      hidden_files = true, -- default: false
+      display_type = true,
+      theme = "dropdown",
+      --   on_project_selected = function(prompt_bufnr)
+    },
+  },
+  -- Добавлял на ура, не факт что это работает
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--ignore-case",
+      "--hidden",
+    },
+    find_command = { "rg", "--files", "--color", "never", "--no-ignore", "--hidden", "--glob", "!.git", "etc", "etc" },
+  },
+}
+require("telescope").load_extension "project"
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-p>",
+  ":lua require'telescope'.extensions.project.project{}<CR>",
+  { noremap = true, silent = true }
+)
+
+-- test
+--vim.keymap.set('n', '<leader>E', '<Cmd>Neotree<CR>', {position=current})
+vim.keymap.set({ "n" }, "<leader>E", ":Neotree position=float toggle<cr>", { desc = "Toggle neotree" })
+--vim.keymap.set({ "n" }, "<leader>x", ":NnnExplorer<cr>", { desc = "Toggle nnn" })
+--vim.keymap.set({"n"}, '<C-b>', '<Cmd>Neotree toggle<CR>')
+--vim.keymap.set({ "i" }, "<C-b>", "<ESC><Cmd>Neotree toggle<CR>")
+vim.keymap.set({ "n" }, "<leader>bk", "<Cmd>bd<CR>", { desc = "kill buffer" })
+--vim.keymap.set("n","<leader>z", ":lua require('zen-mode').toggle({})<cr>", {desc = "Toggle [z]enmode"})
+--vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y', { desc = 'Copy from system clipboard' })
+
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true })
+
+vim.cmd "set timeoutlen=0"
+
+vim.opt.clipboard:append { "unnamed", "unnamedplus" }
+
+-- Как-то для неорга можно использовать
+--vim.api.nvim_create_autocmd("BufNewFile", {
+--  command = "Neorg templates load journal",
+--  pattern = { "/home/ser/org/journal/*.norg" },
+--})
+
+vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true }) -- занято менюшкой
+vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "f", ":HopWord<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", ":HopLineAC<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", ":HopLineBC<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-h>", ":ZenMode<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", ";f", "<leader>f", { silent = true })
+vim.api.nvim_set_keymap("n", ";a", ":Neorg index<cr>", { silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  ";j",
+  ":Neorg journal today<cr>:w<cr>G$a<cr><ESC>:Neorg templates add journal<cr>",
+  --":Neorg journal today<cr>:Neorg templates add journal<cr>",
+  --":Neorg journal today<cr>",
+  { silent = true }
+  --  { desc = "Test desc" }
+) -- set via neorg_leader
+
+vim.api.nvim_set_keymap("n", "J", ":bnext<cr>", { silent = true })
+-- vim.api.nvim_set_keymap("n", "K", ":bprev<cr>", { silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>ss", ":Telescope current_buffer_fuzzy_find<cr>", { silent = true })
+--vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope neorg search_headings<cr>", { silent = true })
+--vim.keymap.set("n", "<lhs>", "<Plug>(neorg.telescope.search_headings)")
+vim.keymap.set("n", "<leader>fg", "<Plug>(neorg.telescope.search_headings)")
+vim.api.nvim_set_keymap("n", ";g", ":Telescope project<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", ";x", ":Telescope project<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", ";n", ":NnnPicker<cr>", { silent = true, desc = "Toggle nnnn" })
+-- vim.cmd("nnoremap Q <nop>") -- don't fuck with Ex mode
+-- vim.cmd("nnoremap j gj") -- don't fuck with line-wrapping
+-- vim.cmd("nnoremap k gk") -- don't fuck with line-wrapping
+-- alternative in lua
+-- Disable the Q command
+-- vim.api.nvim_set_keymap("n", "Q", "<nop>", { noremap = true })
+-- Move by display lines, not actual lines
+-- vim.api.nvim_set_keymap("n", "j", "gj", {})
+-- vim.api.nvim_set_keymap("n", "k", "gk", {})
+-- Move to the first non-blank character of the line
+-- vim.api.nvim_set_keymap("n", "0", "^", { noremap = true })
+
+--vim.cmd "set langmap=ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯЖ;ABCDEFGHIJKLMNOPQRSTUVWXYZ:,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+
+--require "lazy_setup"
+require "polish"
---
+++ .config/nvim/lua/plugins/user.lua
@@ -0,0 +1,222 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- Here are some examples:
+
+return {
+  {
+    "nvim-telescope/telescope-project.nvim",
+    config = function() require("telescope").load_extension "project" end,
+  },
+  {
+    --https://github.com/luukvbaal/nnn.nvim
+    "luukvbaal/nnn.nvim",
+    config = function() require("nnn").setup() end,
+  },
+  { "folke/zen-mode.nvim", cmd = "ZenMode" },
+  { "ivanesmantovich/xkbswitch.nvim", config = function() require("xkbswitch").setup() end },
+  {
+    "nvim-neorg/neorg",
+    dependencies = {
+      {
+        "vhyrro/luarocks.nvim",
+        priority = 1000, -- We'd like this plugin to load first out of the rest
+        config = true,
+      },
+      { "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } },
+      { "nvim-neorg/neorg-telescope" },
+    },
+    version = "*",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.itero"] = {},
+          ["core.export"] = {},
+          ["core.export.markdown"] = {
+            config = {
+              extensions = "all",
+            },
+          },
+          ["core.concealer"] = { config = { icon_preset = "diamond" } },
+          ["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+          ["core.summary"] = {},
+          ["core.journal"] = {
+            config = {
+              strategy = "flat",
+              workspace = "org",
+              --template_name = { "template.norg" },
+              journal_folder = "journal",
+            }, -- Enables support for the journal module
+          },
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+              name = "[neorg]",
+            },
+          }, -- Enables support for completion plugins
+          ["core.integrations.nvim-cmp"] = {},
+          ["core.integrations.telescope"] = {},
+          --["core.mode"] = {}, -- на версии 9.1.1 не работает
+          ["core.integrations.treesitter"] = {},
+          ["core.neorgcmd"] = {},
+          -- ["core.neorgcmd.commands.module.list"] = {},
+          -- ["core.ui.calendar"] = {},
+          ["external.templates"] = {
+            -- templates_dir = "/home/ser/.config/nvim/templates/norg",
+            -- default_subcommand = "add", -- or "fload", "load"
+            -- keywords = { -- Add your own keywords.
+            --   EXAMPLE_KEYWORD = function ()
+            --     return require("luasnip").insert_node(1, "default text blah blah")
+            --   end,
+            -- },
+            -- snippets_overwrite = {},
+          },
+          ["core.keybinds"] = {
+            -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+            config = {
+              --default_keybinds = true,
+              neorg_leader = ";", --"<Leader><Leader>",
+              --neorg_leader = "<Leader><Leader>",
+
+              hook = function(keybinds)
+                local local_leader = keybinds.leader
+                keybinds.map_to_mode("norg", {
+                  n = {
+                    { local_leader .. "jj", ":Neorg journal today<CR>", opts = { desc = "Go to Today's Journal" } },
+                    {
+                      local_leader .. "jh",
+                      ":Neorg journal yesterday<CR>",
+                      opts = { desc = "Go to Yesterday's Journal" },
+                    },
+                    {
+                      local_leader .. "jl",
+                      ":Neorg journal tomorrow<CR>",
+                      opts = { desc = "Go to Tomorrow's Journal" },
+                    },
+                    { local_leader .. "jc", ":Neorg journal custom ", opts = { desc = "Go to Custom Journal" } },
+                  },
+                })
+
+                keybinds.map_to_mode("all", {
+                  n = {
+                    { local_leader .. "a", ":Neorg index<CR>", opts = { desc = "Go to index" } },
+                    { local_leader .. "b", ":Neorg index<CR>", opts = { desc = "Go to index" } },
+                  },
+                })
+              end,
+            },
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                org = "~/.local/src/datagrip/org",
+              },
+              default_workspace = "org",
+            },
+          },
+        },
+      }
+    end,
+  },
+}
+
+-- Пробовал кастомизировать, но на арчкрафте вроде не нужно
+--  {
+--    "nvim-neo-tree/neo-tree.nvim",
+--   -- opts = function(_, opts)
+--   --  opts.filesystem.filtered_items = {
+--   --     hide_gitignored = false,
+--   --     visible = true,
+--   --    hide_dotfiles = false,
+--   --   }
+--   --  --opts.filesystem = {
+--   --  --    window = {
+--   --  --       mappings = {
+--   --  --       -- disable fuzzy finder  (чтобы поиск работал просто по /)
+--   --  --       ["/"] = "noop"
+--   --  --       }
+--   --  --     }
+--   --  -- }
+--   --   return opts
+--   -- end
+--  },
+
+-- ---@type LazySpec
+-- return {
+--
+--   -- == Examples of Adding Plugins ==
+--   "andweeb/presence.nvim",
+--   {
+--     "ray-x/lsp_signature.nvim",
+--     event = "BufRead",
+--     config = function() require("lsp_signature").setup() end,
+--   },
+--
+--   -- == Examples of Overriding Plugins ==
+--   -- customize alpha options
+--   {
+--     "goolord/alpha-nvim",
+--     opts = function(_, opts)
+--       -- customize the dashboard header
+--       opts.section.header.val = {
+--         " █████  ███████ ████████ ██████   ██████",
+--         "██   ██ ██         ██    ██   ██ ██    ██",
+--         "███████ ███████    ██    ██████  ██    ██",
+--         "██   ██      ██    ██    ██   ██ ██    ██",
+--         "██   ██ ███████    ██    ██   ██  ██████",
+--         " ",
+--         "    ███    ██ ██    ██ ██ ███    ███",
+--         "    ████   ██ ██    ██ ██ ████  ████",
+--         "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+--         "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+--         "    ██   ████   ████   ██ ██      ██",
+--       }
+--       return opts
+--     end,
+--   },
+--
+--   -- You can disable default plugins as follows:
+--   { "max397574/better-escape.nvim", enabled = false },
+--
+--   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+--   {
+--     "L3MON4D3/LuaSnip",
+--     config = function(plugin, opts)
+--       require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+--       -- add more custom luasnip configuration such as filetype extend or custom snippets
+--       local luasnip = require "luasnip"
+--       luasnip.filetype_extend("javascript", { "javascriptreact" })
+--     end,
+--   },
+--
+--   {
+--     "windwp/nvim-autopairs",
+--     config = function(plugin, opts)
+--       require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+--       -- add more custom autopairs configuration such as custom rules
+--       local npairs = require "nvim-autopairs"
+--       local Rule = require "nvim-autopairs.rule"
+--       local cond = require "nvim-autopairs.conds"
+--       npairs.add_rules(
+--         {
+--           Rule("$", "$", { "tex", "latex" })
+--             -- don't add a pair if the next character is %
+--             :with_pair(cond.not_after_regex "%%")
+--             -- don't add a pair if  the previous character is xxx
+--             :with_pair(
+--               cond.not_before_regex("xxx", 3)
+--             )
+--             -- don't move right when repeat character
+--             :with_move(cond.none())
+--             -- don't delete if the next character is xx
+--             :with_del(cond.not_after_regex "xx")
+--             -- disable adding a newline when you press <cr>
+--             :with_cr(cond.none()),
+--         },
+--         -- disable for .vim files, but it work for another filetypes
+--         Rule("a", "a", "-vim")
+--       )
+--     end,
+--   },
+-- }
