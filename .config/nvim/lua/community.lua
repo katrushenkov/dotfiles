0a0a8b641cd1f2aebf434b65a33cf7713637c0d5
katrushenkov <katrushenkov.sm@dns-shop.ru>
2024-07-25 13:16:02 +1000

    move to AstroNvim configs

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,16 @@
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.neovide" }, -- Не инициализируется почему-то
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.note-taking.neorg" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- import/override with your plugins folder
+}
