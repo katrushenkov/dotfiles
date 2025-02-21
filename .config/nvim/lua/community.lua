c2fc010a006bb4dc4ba3a0d5a7c7f3df814369b9
Sergey Katrushenkov <katrushenkov.sm@dns-shop.ru>
2025-02-21 16:38:08 +1000

    astronvim: install zen-mode via astrocommunity

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -13,6 +13,7 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.everforest" },
   { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.editing-support.zen-mode-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   --{ import = "astrocommunity.recipes.heirline-clock-statusline" },
   --{ import = "astrocommunity.recipes.heirline-mode-text-statusline" },
