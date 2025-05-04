ddfac734c497b7ef0b3778d89cd7a6eed8886bed
Sergey Katrushenkov <katrushenkov.sm@dns-shop.ru>
2025-05-05 09:41:07 +1000

    astronvim: add puppet parser

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "puppet",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
