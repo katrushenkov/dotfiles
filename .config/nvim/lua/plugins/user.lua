b9c74e3688cceca28d1dc70d6d88c8ac98efadcf
Sergey Katrushenkov <katrushenkov.sm@dns-shop.ru>
2025-05-16 16:06:30 +1000

    nnn: remove bookmarks plugin comment

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -173,7 +173,6 @@ return {
   {
     "Kurren123/mssql.nvim",
     opts = {},
-    -- optional. You also need to call set_keymaps (see below)
     dependencies = { "folke/which-key.nvim" },
   },
   {
