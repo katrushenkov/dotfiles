27f117f6bc805fc249fc236351bb59456080612d
Sergey Katrushenkov <katrushenkov.sm@dns-shop.ru>
2025-05-28 10:41:19 +1000

    nvim: clean init.lua

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -43,6 +43,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = true, -- sets vim.opt.wrap
+        timeoutlen = 0
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
