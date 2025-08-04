c63b43b8547d84bba4e77c2339b2f94d6fc261e2
Sergey Katrushenkov <katrushenkov.sm@dns-shop.ru>
2025-08-04 13:42:15 +1000

    astronvim: add obsidian.nvim

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -29,6 +29,40 @@ return {
     end,
   },
   {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = false,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      legacy_commands = false,
+      ui = {
+        enable = false,
+      },
+      workspaces = {
+        -- {
+          -- name = "personal",
+          -- path = "~/vaults/personal",
+        -- },
+        {
+          name = "work",
+          path = "/home/ser/.local/src/datagrip",
+        },
+      },
+  
+      -- see below for full list of options 👇
+    },
+  },
+  {
     "nvim-neorg/neorg",
     enabled = false,
     dependencies = {
@@ -249,6 +283,7 @@ return {
       },
       picker = {
         ui_select = true,
+        prompt = ' ',
         formatters = {
           file = {
             filename_first = false, -- display filename before the file path
@@ -258,6 +293,28 @@ return {
             git_status_hl = true, -- use the git status highlight group for the filename
           },
         },
+        previewers = {
+                           git = {
+                               native = true,
+                           },
+        },
+        win = {
+                           input = {
+                               keys = {
+                                   -- ['<Tab>'] = { 'list_down', mode = { 'i', 'n' } },
+                                   -- ['<S-Tab>'] = { 'list_up', mode = { 'i', 'n' } },
+                                   ['<c-x>'] = { 'edit_split', mode = { 'i', 'n' } },
+                                   ['<c-u>'] = {
+                                       'preview_scroll_up',
+                                       mode = { 'i', 'n' },
+                                   },
+                                   ['<c-d>'] = {
+                                       'preview_scroll_down',
+                                       mode = { 'i', 'n' },
+                                   },
+                               },
+                           },
+                       },
         sources = {
           explorer = {
             auto_close = false,
