e83434cc98e271872dd3cafde8bc61ba5f16cb08
Sergey Katrushenkov <katrushenkov.sm@dns-shop.ru>
2025-08-05 12:18:39 +1000

    neovim: update snacks config

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -272,6 +272,7 @@ return {
   },
   {
     "folke/snacks.nvim",
+    enabled=true,
     opts = {
       matcher = {
         fuzzy = true,
@@ -282,7 +283,7 @@ return {
         cwd_bonus = false, -- Bonus for items in current directory
       },
       picker = {
-        ui_select = true,
+        ui_select = false,
         prompt = ' ',
         formatters = {
           file = {
@@ -487,6 +488,34 @@ return {
       },
       explorer = {},
     },
+    init = function()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "VeryLazy",
+          callback = function()
+            -- Setup some globals for debugging (lazy-loaded)
+            _G.dd = function(...)
+              Snacks.debug.inspect(...)
+            end
+            _G.bt = function()
+              Snacks.debug.backtrace()
+            end
+            vim.print = _G.dd -- Override print to use snacks for `:=` command
+    
+            -- Create some toggle mappings
+            Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+            Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+            Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+            Snacks.toggle.diagnostics():map("<leader>ud")
+            Snacks.toggle.line_number():map("<leader>ul")
+            Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+            Snacks.toggle.treesitter():map("<leader>uT")
+            Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+            Snacks.toggle.inlay_hints():map("<leader>uh")
+            Snacks.toggle.indent():map("<leader>ug")
+            Snacks.toggle.dim():map("<leader>uD")
+          end,
+        })
+      end,
     keys = {
       -- Top Pickers & Explorer
       { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
