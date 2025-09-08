74ecc99f3cc41489860f58ad46d2aa171e8f0e6b
Sergey Katrushenkov <katrushenkov@gmail.com>
2025-09-08 21:48:15 +1000

    hypr: improve switch windows script

--- .config/hypr/scripts/windows.sh
+++ .config/hypr/scripts/windows.sh
@@ -1,2 +1,19 @@
 # hyprctl clients | awk '/title: ./ { gsub("\t*title: *", ""); print}' | tofi | xargs -I{} hyprctl dispatch focuswindow "title:{}" 
-hyprctl clients | awk '/class: ./ { gsub("\t*class: *", ""); print}' | tofi | xargs -I{} hyprctl dispatch focuswindow "class:{}" 
+# hyprctl clients | awk '/class: ./ { gsub("\t*class: *", ""); print}' | tofi | xargs -I{} hyprctl dispatch focuswindow "class:{}" 
+
+
+hyprctl clients | awk '
+/class: / {
+    gsub(/^[[:space:]]*class: /, "")
+    class = $0
+}
+/title: / {
+    gsub(/^[[:space:]]*title: /, "")
+    title = $0
+    print class " - " title " - " address
+    class = ""; title = ""
+}
+/address: / {
+    gsub(/^[[:space:]]*address: /, "")
+    address = $0
+}' | tofi | awk -F' ' '{print $1}' | xargs -I{} hyprctl dispatch focuswindow "class:{}"
