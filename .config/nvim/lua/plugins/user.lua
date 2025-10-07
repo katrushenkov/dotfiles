bad5ef4e73788da6dc72aa8ec52c5ce92c0d4418
Sergey Katrushenkov <katrushenkov@gmail.com>
2025-10-07 22:43:48 +1000

    neovim: add templates for flash

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -264,6 +264,8 @@ return {
     end,
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump { search = { mode = function(str) return "\\<" .. str end } } end, desc = "Flash",}, -- match beginnings of the words only
+      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump({ pattern = vim.fn.expand("<cword>") }) end, desc = "Flash"}, -- with the word under the cursor
       { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
