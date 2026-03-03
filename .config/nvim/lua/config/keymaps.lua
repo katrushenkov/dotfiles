a95c5766235794e59d81514dede3c05b13a8508b
Sergey Katrushenkov <katrushenkov@gmail.com>
2026-03-03 15:17:07 +1000

    nvim: clean keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
---delete
---#vim.keymap.set('n', '<F13>', '<Esc>')
---#vim.keymap.set('i', '<F13>', '<Esc>')
---#vim.keymap.set('v', '<F13>', '<Esc>')
-
 local opts = { noremap = true, silent = true }
+
+vim.keymap.set(
+  "n",
+  "<leader>k",
+  '<cmd>lua require("kubectl").toggle({ tab = true })<cr>',
+  { noremap = true, silent = true }
+)
 
 vim.keymap.set({"n"},";J","<Cmd>edit $HOME/.local/src/datagrip/journal/journal.md<cr>G<ESC>",{ silent = true, desc = "Show journal" })
 
@@ -17,34 +19,26 @@ vim.api.nvim_del_keymap("n", "<leader>qq")
 vim.keymap.set({ "n" }, ";z", ":qa!<cr>", { silent = true, desc = "Quit without save" })
 vim.keymap.set({ "n" }, "<leader>q", ":qa!<cr>", { silent = true, desc = "Quit without save" })
 
-vim.keymap.set("n", "zn", "zR") -- open all (n = "open" в qwerty рядом)
-vim.keymap.set("n", "zm", "zM") -- close all (original mnemo)
+vim.keymap.set("n", "zn", "zR") -- open all
+vim.keymap.set("n", "zm", "zM") -- close all
 vim.keymap.set("n", ";z", "za")
 
 vim.keymap.set({ "n" }, ";q", ":qa!<cr>", { silent = true, desc = "Quit without save" })
 --vim.keymap.set({ "n" }, "<leader>q", "<Cmd>qa<CR>", { desc = "Quit all" })
 
-vim.keymap.set("v", "<leader>yy", function()
-  require("yeet").execute_selection("source venv/bin/activate", { clear_before_yeet = true })
-end)
-
 -- Tab navigation
 vim.keymap.set({"n"}, "<s-tab>", "<cmd>tabnew %<cr>", opts)
-vim.keymap.set({ "n" }, "<s-h>", "<cmd>tabp<cr>", opts)
-vim.keymap.set({ "n" }, "<s-l>", "<cmd>tabn<cr>", opts)
+vim.keymap.set({"n"}, "<s-h>", "<cmd>tabp<cr>", opts)
+vim.keymap.set({"n"}, "<s-l>", "<cmd>tabn<cr>", opts)
 
 vim.keymap.set("n", ";g", function()
   require("snacks").picker.files({
     cwd = vim.fn.expand("~/.config/nnn/bookmarks"),
-    hidden = true, -- показывать скрытые файлы
+    hidden = true,
     follow_symlinks = true,
     respect_gitignore = true,
   })
 end, { desc = "Shortcuts picker" })
-
---Автоматически использовать системный буфер обмена для копирования и вставки
---vim.keymap.set("n", "y", '"+y') -- Копировать в системный буфер обмена
---vim.keymap.set("n", "p", '"+p') -- Вставить из системного буфера обмена
 
 vim.keymap.set("n", "<leader>bk", "<Cmd>confirm bd<CR>", { desc = "kill buffer" })
 vim.keymap.set("n", "<S-Tab>", ":bprev<CR>", { noremap = true })
@@ -55,24 +49,8 @@ vim.keymap.set("n", ";a", ":edit ~/.local/src/datagrip/index.md<cr>", { silent =
 vim.keymap.set("n", "J", ":bnext<cr>", { silent = true })
 vim.keymap.set("n", ";h", ":chdir ~/.local/src/datagrip<cr>", { silent = true })
 vim.keymap.set("n", ";y", ":silent %y+<cr>", { silent = true, desc = "Yank the whole buffer" })
---  keymap.set("n", "<leader>y", "<cmd>%yank<cr>", { desc = "yank entire buffer" })
 
---vim.keymap.set({ "n" }, "<leader>x", ":NnnExplorer<cr>", { desc = "Toggle nnn" })
---vim.keymap.set({"n"}, '<C-b>', '<Cmd>Neotree toggle<CR>')
---vim.keymap.set({ "i" }, "<C-b>", "<ESC><Cmd>Neotree toggle<CR>")
---vim.keymap.set("n","<leader>z", ":lua require('zen-mode').toggle({})<cr>", {desc = "Toggle [z]enmode"})
---vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y', { desc = 'Copy from system clipboard' })
---vim.cmd("nnoremap Q <nop>") -- don't fuck with Ex mode
---vim.cmd("nnoremap j gj") -- don't fuck with line-wrapping
---vim.cmd("nnoremap k gk") -- don't fuck with line-wrapping
---alternative in lua
---Disable the Q command
---vim.api.nvim_set_keymap("n", "Q", "<nop>", { noremap = true })
---Move by display lines, not actual lines
---vim.api.nvim_set_keymap("n", "j", "gj", {})
---vim.api.nvim_set_keymap("n", "k", "gk", {})
-
---Move to the first non-blank character of the line
+-- Move to the first non-blank character of the line
 vim.keymap.set("n", "0", "^", { noremap = true })
 
 -- Sudo write
@@ -119,5 +97,3 @@ function JOURNAL_ADD()
 end
 
 vim.keymap.set("n", ";j", "<cmd>lua JOURNAL_ADD()<CR>", opts)
-
---{ noremap = true, silent = false, desc = "test"})
