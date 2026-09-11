b91c47f7b654fa59bbd4a6160cc3d961c1f6606a
Sergey Katrushenkov <katrushenkov@gmail.com>
2026-09-12 02:23:20 +1000

    nvim: update live_grep snacks bindings

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -386,7 +386,8 @@ return {
       { "<leader>go", function() Snacks.gitbrowse() end,           desc = "Git Browse (open in browser)" },
       -- Grep
       { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
-      { "<leader>sg", LazyVim.pick("live_grep"),                   desc = "Grep (Root Dir)" },
+      { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+      { "<leader>sG", LazyVim.pick("live_grep"),                   desc = "Grep (Root Dir)" },
       { "<leader>sw", LazyVim.pick("grep_word"),                   desc = "Visual selection or word (Root Dir)", mode = { "n", "x" } },
       { "<leader>sW", LazyVim.pick("grep_word", { root = false }), desc = "Visual selection or word (cwd)", mode = { "n", "x" } },
       -- search
